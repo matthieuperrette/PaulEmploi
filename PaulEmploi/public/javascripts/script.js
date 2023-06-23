88bf800b603a31c7130e1b36adf1d5be54d03873
tbridoux <thomas.bridoux@etu.utc.fr>
2023-06-23 15:13:08 +0200

    boutton en addEventListener

--- PaulEmploi/public/javascripts/script.js
+++ PaulEmploi/public/javascripts/script.js
@@ -2,8 +2,8 @@ var btn_connexion = document.getElementById("btn_connexion");
 var btn_inscription= document.getElementById("btn_inscription");
 
 
-//btn_connexion.addEventListener('click', toggleForms);
-//btn_inscription.addEventListener('click', toggleForms);
+btn_connexion.addEventListener('click', toggleConnexion);
+btn_inscription.addEventListener('click', toggleInscription);
 
 function toggleConnexion() {
     var form_connexion = document.getElementById("form_connexion");
